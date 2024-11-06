76252e1fd17eb84644d43e15f177a46c90e5e9da
Serhii Taran <serg.taran1970@gmail.com>
2024-11-06 21:20:30 +0200

    js-mod9-all

---
+++ src/js/2-form.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message"]');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    formData.email = parsedData.email || '';
+    formData.message = parsedData.message || '';
+    emailInput.value = formData.email;
+    messageTextarea.value = formData.message;
+  }
+});
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+
+  formData[name] = value.trim();
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+  formData.email = '';
+  formData.message = '';
+
+  emailInput.value = '';
+  messageTextarea.value = '';
+});
